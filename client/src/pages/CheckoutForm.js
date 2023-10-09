5d0ef0e2c1b11f3d98afa9e287e6cc4d9eb6e1d7
vaishu567 <rslv3vaishu567@gmail.com>
2023-10-09 16:30:33 +0530

    modified final

--- client/src/pages/CheckoutForm.js
+++ client/src/pages/CheckoutForm.js
@@ -61,7 +61,7 @@ export default function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: `http://localhost:8080/order-success/${currentOrder.id}`,
+        return_url: `https://e-shopfit-rslv3vaishu567-gmailcom.vercel.app/order-success/${currentOrder.id}`,
       },
     });
 
